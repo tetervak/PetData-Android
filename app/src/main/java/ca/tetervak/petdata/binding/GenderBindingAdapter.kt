28734bc2acf2d4d792b2226ea56fd81bf1b45453
Alex Tetervak <tetervak@sheridancollege.ca>
2020-12-08 14:24:18 -0500

    made Gender Binding Adapter

---
+++ app/src/main/java/ca/tetervak/petdata/binding/GenderBindingAdapter.kt
@@ -0,0 +1,14 @@
+package ca.tetervak.petdata.binding
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import ca.tetervak.petdata.R
+import ca.tetervak.petdata.domain.Gender
+
+@BindingAdapter("gender")
+fun bindGender(textView: TextView, gender: Gender?) {
+    if (gender is Gender) {
+        val catMessages = textView.resources.getStringArray(R.array.genders)
+        textView.text = catMessages[gender.ordinal]
+    }
+}
