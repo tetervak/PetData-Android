8f74f9983dcfee94f78cc15ac6911998a431f6c0
Alex Tetervak <tetervak@sheridancollege.ca>
2024-10-07 22:06:10 -0400

    added the nav host

--- app/src/main/java/ca/tetervak/petdata/ui/AppRootScreen.kt
+++ app/src/main/java/ca/tetervak/petdata/ui/AppRootScreen.kt
@@ -1,10 +1,22 @@
 package ca.tetervak.petdata.ui
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import ca.tetervak.petdata.ui.petlist.PetListScreen
 
 @Composable
-fun AppRootScreen(){
+fun AppRootScreen() {
 
-    PetListScreen()
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "pet-list"
+    ) {
+        composable(route = "pet-list") {
+            PetListScreen()
+        }
+
+    }
 }
