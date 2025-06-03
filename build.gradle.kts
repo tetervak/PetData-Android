250e469fd27c5e8c590d6151d80650271cd962d4
Alex Tetervak <tetervak@sheridancollege.ca>
2025-06-02 22:06:06 -0400

    updated libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 
-    // needed for the dependency injection
-    id("com.google.dagger.hilt.android") version "2.48.1" apply false
+    alias(libs.plugins.kotlin.compose) apply false
+
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.hilt.android) apply false
 }
